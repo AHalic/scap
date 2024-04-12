ef156f67dcfd89e5388ccc6a976049ed19a8e439
Sophie Dilhon <sophie.gama@edu.ufes.br>
2024-04-12 00:00:10 -0400

    :refactor: Modifica tipos no Filtro de afastamento

--- code/lib/interfaces/Filtros.ts
+++ code/lib/interfaces/Filtros.ts
@@ -7,7 +7,6 @@ enum TipoPessoa {
 
 interface FiltrosPessoa {
 	nome: string | null;
-	sobrenome: string | null;
 	email: string | null;
 	telefone: string | null;
 	tipo: TipoPessoa | null;
@@ -16,15 +15,54 @@ interface FiltrosPessoa {
 interface FiltrosAfastamento {
 	estado: EstadoSolicitacao | null;
 	onus: Onus | null;
-	dataSolicitacao: Date | null;
-	dataInicio: Date | null;
-	dataFim: Date | null;
-	dataInicioEvento: Date | null;
-	dataFimEvento: Date | null;
-	solicitanteId: string | null;
+	dataSolicitacao: string | null;
+	dataInicio: string | null;
+	dataFim: string | null;
+	dataInicioEvento: string | null;
+	dataFimEvento: string | null;
+	solicitante: string | null;
 	tipo: TipoAfastamento | null;
 }
 
+const estadoAfastamentoColors = {
+	[EstadoSolicitacao.INICIADO]: {
+		estado: EstadoSolicitacao.INICIADO,
+		color: "indigo",
+	},
+	[EstadoSolicitacao.BLOQUEADO]: {
+		estado: EstadoSolicitacao.BLOQUEADO,
+		color: "yellow",
+	},
+	[EstadoSolicitacao.LIBERADO]: {
+		estado: EstadoSolicitacao.LIBERADO,
+		color: "teal",
+	},
+	[EstadoSolicitacao.APROVADO_DI]: {
+		estado: EstadoSolicitacao.APROVADO_DI,
+		color: "blue",
+	},
+	[EstadoSolicitacao.APROVADO_CT]: {
+		estado: EstadoSolicitacao.APROVADO_CT,
+		color: "cyan",
+	},
+	[EstadoSolicitacao.APROVADO_PRPPG]: {
+		estado: EstadoSolicitacao.APROVADO_PRPPG,
+		color: "sky",
+	},
+	[EstadoSolicitacao.ARQUIVADO]: {
+		estado: EstadoSolicitacao.ARQUIVADO,
+		color: "green",
+	},
+	[EstadoSolicitacao.CANCELADO]: {
+		estado: EstadoSolicitacao.CANCELADO,
+		color: "zinc",
+	},
+	[EstadoSolicitacao.REPROVADO]: {
+		estado: EstadoSolicitacao.REPROVADO,
+		color: "red",
+	},
+};
+
 export type { FiltrosPessoa, FiltrosAfastamento };
-export { TipoPessoa };
+export { TipoPessoa, estadoAfastamentoColors };
 
