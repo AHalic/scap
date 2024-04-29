cc7b840910995def1781cfd20aa94d5dde079160
Sophie Dilhon <sophie.gama@edu.ufes.br>
2024-04-29 16:51:55 -0400

    :recycle: Modifica ProtectedRoute para suportar 404

--- code/src/auth.tsx
+++ code/src/auth.tsx
@@ -1,19 +1,26 @@
+import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 import LoadingPage from "./components/LoadingPage";
 
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+const ProtectedRoute = ({ isSecretarioExclusive=false, children }: { isSecretarioExclusive?: boolean,children: React.ReactNode }) => {
 	const router = useRouter();
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		// Check if user is authenticated, e.g., by checking the presence of the authentication token
-		const isAuthenticated = !!document.cookie.includes("session");
-
-		if (!isAuthenticated) {
+		// const isAuthenticated = !!document.cookie.includes("session");
+		const session = Cookies.get('session');
+		const user = session ? JSON.parse(session) : undefined;
+		
+		
+		if (!user) {
 			// If user is not authenticated, redirect to login page
 			router.push("/");
+		} else if (isSecretarioExclusive && !user?.secretarioId) {
+			// If user is authenticated but is not a secretario, redirect to 404 page
+			router.push("/404");
 		} else {
 			setLoading(false);
 		}
@@ -32,9 +39,11 @@ export const RedirectLogin = ({ children }: { children: React.ReactNode }): Reac
 
 	useEffect(() => {
 		// Check if user is authenticated, e.g., by checking the presence of the authentication token
-		const isAuthenticated = !!document.cookie.includes("session");
+		// const isAuthenticated = !!document.cookie.includes("session");
+		const session = Cookies.get('session');
+		const user = session ? JSON.parse(session) : undefined;
 		
-		if (isAuthenticated) {
+		if (user) {
 			// If user is authenticated, redirect to afastamentos page
 			router.push("/afastamento");
 		} else {
