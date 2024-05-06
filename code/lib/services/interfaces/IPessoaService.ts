b0a0e2b6c0a906d910c629d991c515f7fc94c9e7
Sophie Dilhon <sophie.gama@edu.ufes.br>
2024-05-06 12:59:53 -0400

    :recycle: Troca tipos na interface de PessoaService

--- code/lib/services/interfaces/IPessoaService.ts
+++ code/lib/services/interfaces/IPessoaService.ts
@@ -1,10 +1,10 @@
 import { Pessoa } from "@prisma/client";
 
-import { FiltrosPessoa } from "../../interfaces/Filtros";
+import { FiltrosPessoa, PessoaCompleta } from "../../interfaces/Filtros";
 
 export interface IPessoaService {
 	login(email: string, senha: string): Promise<Pessoa | null>;
-	criar(data: Pessoa, userId: string): Promise<Pessoa>;
+	criar(data: PessoaCompleta, userId: string): Promise<Pessoa>;
 	editar(data: Pessoa, userId: string): Promise<Pessoa>;
 	deletar(id: string, userId: string): Promise<Pessoa>;
 	buscarPorId(id: string): Promise<Pessoa | null>;
